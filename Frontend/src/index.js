8620e9eb9730af50a50659df2a1acc6e9056202a
Abhay Arora <abhayarorasap123@gmail.com>
2022-06-26 02:38:24 +0530

    Added Dashboard Page
    The page displays the analytics to an influencer and also displays the newly arrived and existing leads on the website

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -13,6 +13,7 @@ import Homepage from "./components/Homepage/Homepage";
 import SignUp from "./components/SignUp/SignUp";
 import VerificationPage from "./components/VerificationPage/VerificationPage";
 import Options from "./components/Options/Options";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -27,6 +28,7 @@ root.render(
         <Route exact path="/leaderboard" element={<Leaderboard />} />
         <Route exact path="/verify" element={<VerificationPage />} />
         <Route exact path="/options" element={<Options />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
         <Route exact path="/channels/:id" element={<Home />} />
       </Routes>
     </Provider>
