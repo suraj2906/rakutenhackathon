5e4111f0f2f266db9138334af6308fa611ca2835
Abhay Arora <abhayarorasap123@gmail.com>
2022-06-26 05:37:23 +0530

    Replaced emojis to emoji's of the respective id's
    It would give a better interpretation for the user's interests

--- Frontend/src/components/Profile/Profile.js
+++ Frontend/src/components/Profile/Profile.js
@@ -5,7 +5,7 @@ import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router-dom";
 import {setChannelInfo} from "../../features/channelSlice";
 
-const Profile = ({id, name, type}) => {
+const Profile = ({id, name}) => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const setChannel=() =>{
@@ -20,8 +20,21 @@ const Profile = ({id, name, type}) => {
         <div
             className='profile__container flex align-middle px-6 py-3 scrollbar-hide min-h-[1rem] cursor-pointer overflow-hidden transition-all ease-out duration-200' onClick={setChannel}
         >
-            <img className={`${type} h-10 w-10 profile__img`}
-                 src="https://thumbs.dreamstime.com/b/rainbow-love-heart-background-red-wood-60045149.jpg" alt=""/>
+            {/*<img className={`${type} h-10 w-10 profile__img`}*/}
+            {/*     src="https://thumbs.dreamstime.com/b/rainbow-love-heart-background-red-wood-60045149.jpg" alt=""/>*/}
+
+
+            <div className="main">
+                <div className="flex flex-row">
+                <div className="d1">🧬</div>
+                <div className="d1">🩺</div>
+                </div>
+                <div className="flex flex-row">
+                    <div className="d1">🚗</div>
+                    <div className="d1">🏚️</div>
+                </div>
+            </div>
+
             <span className='profile-name px-4 align-middle place-self-center'>{name}</span>
         </div>
     );
